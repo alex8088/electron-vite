714798fd5697bfb992b360cf8735ea3b10decc15
alex8088 <244096523@qq.com>
2022-09-09 01:28:36 +0800

    chore: disable build.watch in production

--- src/build.ts
+++ src/build.ts
@@ -9,14 +9,23 @@ export async function build(inlineConfig: InlineConfig = {}): Promise<void> {
   if (config.config) {
     const mainViteConfig = config.config?.main
     if (mainViteConfig) {
+      if (mainViteConfig.build?.watch) {
+        mainViteConfig.build.watch = null
+      }
       await viteBuild(mainViteConfig)
     }
     const preloadViteConfig = config.config?.preload
     if (preloadViteConfig) {
+      if (preloadViteConfig.build?.watch) {
+        preloadViteConfig.build.watch = null
+      }
       await viteBuild(preloadViteConfig)
     }
     const rendererViteConfig = config.config?.renderer
     if (rendererViteConfig) {
+      if (rendererViteConfig.build?.watch) {
+        rendererViteConfig.build.watch = null
+      }
       await viteBuild(rendererViteConfig)
     }
   }
