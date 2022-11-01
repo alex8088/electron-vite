97ad5fa7260f3e3ab34b098c010835bf3f88f31f
alex8088 <244096523@qq.com>
2022-11-02 01:34:19 +0800

    feat: make a SWC plugin to emit decorator metadata (#48)

---
+++ src/swc.ts
@@ -0,0 +1,112 @@
+import { createRequire } from 'node:module'
+import type { SourceMap } from 'rollup'
+import type { TransformConfig, Output as TransformResult, JscConfig } from '@swc/core'
+import type { Plugin, UserConfig, FilterPattern } from 'vite'
+import { createFilter } from 'vite'
+
+type SwcTransformResult = Omit<TransformResult, 'map'> & {
+  map: SourceMap
+}
+
+type SwcTransformOptions = {
+  sourcemap?: boolean | 'inline' | undefined
+  minify?: boolean
+} & TransformConfig
+
+async function transformWithSWC(code: string, id: string, options: SwcTransformOptions): Promise<SwcTransformResult> {
+  const { sourcemap = false, minify = false } = options
+
+  delete options.sourcemap
+  delete options.minify
+
+  const isTs = /\.tsx?$/.test(id)
+
+  const require = createRequire(import.meta.url)
+
+  const swc: typeof import('@swc/core') = require('@swc/core')
+
+  if (!swc) {
+    throw new Error('swc plugin require @swc/core, you need to install it.')
+  }
+
+  const jsc: JscConfig = {
+    parser: {
+      syntax: isTs ? 'typescript' : 'ecmascript',
+      decorators: true
+    },
+    transform: {
+      legacyDecorator: true,
+      decoratorMetadata: true,
+      ...options
+    },
+    keepClassNames: true,
+    target: 'es2022',
+    minify: {
+      format: {
+        comments: false
+      }
+    }
+  }
+
+  const result = await swc.transform(code, {
+    jsc,
+    sourceMaps: sourcemap,
+    minify,
+    configFile: false,
+    swcrc: false
+  })
+
+  const map: SourceMap = sourcemap && result.map ? JSON.parse(result.map) : { mappings: '' }
+
+  return {
+    code: result.code,
+    map
+  }
+}
+
+export type SwcOptions = {
+  include?: FilterPattern
+  exclude?: FilterPattern
+  transformOptions?: TransformConfig
+}
+
+export function swcPlugin(options: SwcOptions = {}): Plugin {
+  const filter = createFilter(options.include || /\.(m?ts|[jt]sx)$/, options.exclude || /\.js$/)
+  let sourcemap: boolean | 'inline' = false
+  let minify: boolean | 'esbuild' | 'terser' = false
+  return {
+    name: 'vite:swc',
+    config(): UserConfig {
+      return {
+        esbuild: false
+      }
+    },
+    async configResolved(resolvedConfig): Promise<void> {
+      sourcemap = resolvedConfig.build?.sourcemap === 'inline' ? 'inline' : !!resolvedConfig.build?.sourcemap
+      minify = resolvedConfig.build?.minify
+    },
+    async transform(code, id): Promise<void | { code: string; map: SourceMap }> {
+      if (filter(id)) {
+        const result = await transformWithSWC(code, id, { sourcemap, ...(options.transformOptions || {}) })
+        return {
+          code: result.code,
+          map: result.map
+        }
+      }
+    },
+    async renderChunk(code, chunk): Promise<null | { code: string; map: SourceMap }> {
+      if (!minify || minify === 'terser') {
+        return null
+      }
+      const result = await transformWithSWC(code, chunk.fileName, {
+        sourcemap,
+        minify: true,
+        ...(options.transformOptions || {})
+      })
+      return {
+        code: result.code,
+        map: result.map
+      }
+    }
+  }
+}
