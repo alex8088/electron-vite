3b2ba0ae25868076052f67a112982281635af04a
alex8088 <244096523@qq.com>
2023-04-18 23:11:33 +0800

    chore: remove process env define

--- src/plugins/electron.ts
+++ src/plugins/electron.ts
@@ -29,14 +29,6 @@ function findInput(root: string, scope = 'renderer'): string {
   return ''
 }
 
-function processEnvDefine(): Record<string, string> {
-  return {
-    'process.env': `process.env`,
-    'global.process.env': `global.process.env`,
-    'globalThis.process.env': `globalThis.process.env`
-  }
-}
-
 export function electronMainVitePlugin(options?: ElectronPluginOptions): Plugin[] {
   return [
     {
@@ -82,9 +74,6 @@ export function electronMainVitePlugin(options?: ElectronPluginOptions): Plugin[
 
         const buildConfig = mergeConfig(defaultConfig.build, build)
         config.build = buildConfig
-
-        config.define = config.define || {}
-        config.define = { ...processEnvDefine(), ...config.define }
 
         config.envPrefix = config.envPrefix || 'MAIN_VITE_'
 
@@ -184,9 +173,6 @@ export function electronPreloadVitePlugin(options?: ElectronPluginOptions): Plug
 
         const buildConfig = mergeConfig(defaultConfig.build, build)
         config.build = buildConfig
-
-        config.define = config.define || {}
-        config.define = { ...processEnvDefine(), ...config.define }
 
         config.envPrefix = config.envPrefix || 'PRELOAD_VITE_'
 
