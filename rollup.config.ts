ff23f7d44d2de383c3ce540c4ed27c68fabd1fa2
alex8088 <244096523@qq.com>
2024-04-21 21:46:06 +0800

    chore: use rollup-plugin-rm to clean dist

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,32 +1,15 @@
 import { createRequire } from 'node:module'
-import fs from 'node:fs/promises'
-import { type Plugin, defineConfig } from 'rollup'
+import { defineConfig } from 'rollup'
 import ts from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import dts from 'rollup-plugin-dts'
+import rm from 'rollup-plugin-rm'
 
 const require = createRequire(import.meta.url)
 const pkg = require('./package.json')
 
 const external = ['esbuild', ...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})]
-
-function clean(when: 'buildStart' | 'buildEnd', target: string): Plugin {
-  const _clean = async (target): Promise<void> => {
-    await fs.rm(target, { recursive: true, force: true }).catch(() => {})
-  }
-  return {
-    name: 'clean',
-    buildStart: async (): Promise<void> => {
-      if (when !== 'buildStart') return
-      await _clean(target)
-    },
-    buildEnd: async (): Promise<void> => {
-      if (when !== 'buildEnd') return
-      await _clean(target)
-    }
-  }
-}
 
 export default defineConfig([
   {
@@ -47,7 +30,7 @@ export default defineConfig([
     ],
     external,
     plugins: [
-      clean('buildStart', 'dist'),
+      rm('dist', 'buildStart'),
       json(),
       ts({ compilerOptions: { rootDir: 'src', declaration: true, declarationDir: 'dist/types' } }),
       resolve()
@@ -59,6 +42,6 @@ export default defineConfig([
   {
     input: 'dist/types/index.d.ts',
     output: [{ file: pkg.types, format: 'es' }],
-    plugins: [dts(), clean('buildEnd', 'dist/types')]
+    plugins: [dts(), rm('dist/types', 'buildEnd')]
   }
 ])
