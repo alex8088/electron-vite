a4f76937129d8dcaf97a645b7218073c6afa2097
alex8088 <244096523@qq.com>
2025-10-29 00:08:03 +0800

    perf: build compatibility target for Electron 39

--- src/electron.ts
+++ src/electron.ts
@@ -69,6 +69,7 @@ export function getElectronNodeTarget(): string {
   const electronVer = getElectronMajorVer()
 
   const nodeVer = {
+    '39': '22.20',
     '38': '22.19',
     '37': '22.16',
     '36': '22.14',
@@ -99,6 +100,7 @@ export function getElectronChromeTarget(): string {
   const electronVer = getElectronMajorVer()
 
   const chromeVer = {
+    '39': '142',
     '38': '140',
     '37': '138',
     '36': '136',
