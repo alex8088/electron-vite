25b10f3c3dfbe6003de98b826253e230b03239e1
Samuel Sergi <sam@sammysergi.com>
2025-12-24 14:20:29 +1100

    feat: support custom Electron package name

--- src/config.ts
+++ src/config.ts
@@ -97,6 +97,13 @@ export interface RendererViteConfig extends BaseViteConfig<RendererBuildOptions>
 
 export interface UserConfig {
   /**
+   * Custom Electron package name (e.g., '@overwolf/ow-electron').
+   * Can also be set via ELECTRON_PKG_NAME environment variable.
+   *
+   * @default 'electron'
+   */
+  electronPackage?: string
+  /**
    * Vite config options for electron main process
    *
    * @see https://vitejs.dev/config/
@@ -310,6 +317,16 @@ export async function resolveConfig(
       userConfig = loadResult.config
       configFile = loadResult.path
       configFileDependencies = loadResult.dependencies
+    }
+  }
+
+  // Set ELECTRON_PKG_NAME from config or environment variable
+  // Priority: existing env var > config file > default to 'electron'
+  if (!process.env.ELECTRON_PKG_NAME) {
+    if (userConfig?.electronPackage) {
+      process.env.ELECTRON_PKG_NAME = userConfig.electronPackage
+    } else {
+      process.env.ELECTRON_PKG_NAME = 'electron'
     }
   }
 
